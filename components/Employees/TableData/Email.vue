5ee6768c6dbe936374916cf162ffbb2cf0f3ab22
opprogrammer <97902777+opprogrammer@users.noreply.github.com>
2023-06-30 17:24:40 +0530

    Delete components directory

--- components/Employees/TableData/Email.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div class="px-3 py-4">
-		<p class="text-black truncate">{{ content.primaryEmail }}</p>
-	</div>
-</template>
-
-<script setup>
-	const { content } = defineProps(["content"]);
-</script>
